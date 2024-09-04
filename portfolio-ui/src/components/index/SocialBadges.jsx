48aa90da17de0bacc90bc6c104cdc1db1733fe5a
mads-bohn <mady.bohn@gmail.com>
2024-09-04 15:12:44 -0500

    created and styled SocialBadges component

---
+++ portfolio-ui/src/components/index/SocialBadges.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+export default function SocialBadges({iconSize}) {
+  return (
+    <div className='socialBadges'>
+        <a href='https://github.com/mads-bohn'>
+          <FontAwesomeIcon icon={faGithub} size={iconSize} className='socialIcon'/>
+        </a>
+        <a href='https://www.linkedin.com/in/mads-bohn-12a9212b0/'>
+          <FontAwesomeIcon icon={faLinkedin} size={iconSize} className='socialIcon' />
+        </a>
+        <a href='mailto:madsbohn14@gmail.com'>
+          <FontAwesomeIcon icon={faEnvelope} size={iconSize} className='socialIcon' />
+        </a>   
+    </div>
+  )
+}
--- portfolio-ui/src/pages/index.jsx
+++ portfolio-ui/src/pages/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import SocialBadges from '../components/index/SocialBadges'
 
 export default function Index() {
   return (
     <div>
         <Navbar />
         <p>Hello, world!</p>
+        <SocialBadges iconSize={"l"} />
     </div>
   )
 }
