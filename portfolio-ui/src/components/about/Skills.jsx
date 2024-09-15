2c9f5c823ed10730e3cbff796fc7f1f4097a8514
mads-bohn <mady.bohn@gmail.com>
2024-09-15 14:56:04 -0500

    created intro and skills components for about page

---
+++ portfolio-ui/src/components/about/Skills.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Skills() {
+  return (
+    <div className='about-skills'>
+    <h2>Skills</h2>
+    <ul>
+      <li>JavaScript</li>
+      <li>Java</li>
+      <li>SQL</li>
+      <li>React</li>
+      <li>Spring Boot</li>
+      <li>HTML + CSS</li>
+    </ul>
+  </div>
+  )
+}
--- portfolio-ui/src/pages/About.jsx
+++ portfolio-ui/src/pages/About.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import SideBar from '../components/navigation/SideBar'
+import Skills from '../components/about/Skills'
+import Intro from '../components/about/Intro'
 
 export default function About() {
   return (
     <div>
       <SideBar />
-      <h1>About</h1>
+      <div className='about'>
+        <Intro />
+        <Skills />
+      </div> 
     </div>
   )
 }
