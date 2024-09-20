7f4161631fd53210708bfacd4aecde0d1aa449f6
mads-bohn <mady.bohn@gmail.com>
2024-09-19 21:38:12 -0500

    added bio info

---
+++ portfolio-ui/src/components/about/Bio.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Bio() {
+  return (
+    <div className='bio'>
+        <h1>About</h1>
+        <h3 className='bio-highlight'>Hi, I'm Mads. I'm a self-motivated and curious full-stack developer with a history
+          in customer-facing environments. 
+        </h3>
+        <p className='bio-history'>My passion for coding began when I wanted to create my own website, and became frustrated
+          with the constraints of available no-code websites. Programming quickly reignited my desire
+          to learn and solve problems. In 2024, I graduated from LaunchCode's web development course.
+          Now, I'm looking for more opportunities to expand my existing skill set in software development. 
+        </p>
+    </div>
+  )
+}
--- portfolio-ui/src/pages/About.jsx
+++ portfolio-ui/src/pages/About.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import SideBar from '../components/navigation/SideBar'
 import Skills from '../components/about/Skills'
-import Intro from '../components/about/Intro'
+import Bio from '../components/about/Bio'
 
 export default function About() {
   return (
     <div>
       <SideBar />
       <div className='about'>
-        <Intro />
+        <Bio />
         <Skills />
       </div> 
     </div>
