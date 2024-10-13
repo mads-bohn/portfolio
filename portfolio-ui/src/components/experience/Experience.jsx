7f8c9ef306d4198732d7bd131e4f34b9d4388902
mads-bohn <mady.bohn@gmail.com>
2024-10-13 16:34:39 -0500

    created experienceArray and mapped data to html format

--- portfolio-ui/src/components/experience/Experience.jsx
+++ portfolio-ui/src/components/experience/Experience.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ResumeButton from './ResumeButton'
 
+let experienceArray = [
+    {
+        title: "LaunchCode",
+        years: "2024",
+        description: "Here's a paragraph about what I learned in LaunchCode."
+    },
+    {
+        title: "Service Industry",
+        years: "2016-2022",
+        description: "Here's a paragraph about why my experience in the service industry makes me a great programmer."
+    }
+]
+
 export default function Experience() {
   return (
     <div>
-        <p>All my experience</p>
+        <div className='experience-list'>
+            {experienceArray.map(experience => (
+                <div className='project' key={experience.title}>
+                    <p className='experience-years'>{experience.years}</p>
+                    <div className='project-text'>
+                        <h3 className='project-title'>{experience.title}</h3>
+                        <p className='project-description'>{experience.description}</p>
+                    </div>
+                </div>
+            )
+            )}
+        </div>
         <ResumeButton />
     </div>
   )
